ef1b037cae85a51e83b343924694c3c5ee83d02d
ezcan <kuanjunglien@gmail.com>
2021-04-21 22:43:12 +0800

    fix css path

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~assets/css/_variables.scss'],
+  css: ['./assets/css/_variables.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
