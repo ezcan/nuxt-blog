3bf8b1e642de8d384ae47018e92aa2b018878791
ezcan <kuanjunglien@gmail.com>
2021-04-21 11:59:38 +0800

    first generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/nuxt/blog/',
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-blog',
